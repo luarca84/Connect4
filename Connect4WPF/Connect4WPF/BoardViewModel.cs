38569018f23613820be84738a0ac431635091c4c
Diego <luarca84@gmail.com>
2016-02-15 21:10:26 +0100

    Added basic Connect4WPF layout

---
+++ Connect4WPF/Connect4WPF/BoardViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4WPF
+{
+    public class BoardViewModel
+    {
+        public BoardViewModel()
+        {
+            var rand = new Random();
+            Squares = Enumerable
+                .Range(1, 42)
+                .Select(a => new SquareViewModel() { Token = rand.Next(-1, 2) })
+                .ToList();
+        }
+
+        public List<SquareViewModel> Squares { get; set; }
+    }
+}
---
+++ Connect4WPF/Connect4WPF/SquareViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4WPF
+{
+    public class SquareViewModel : INotifyPropertyChanged
+    {
+        private int _Token;
+        public int Token
+        {
+            get
+            {
+                return _Token;
+            }
+            set
+            {
+                if (_Token.Equals(value)) return;
+
+                _Token = value;
+                RaisePropertyChanged("Token");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            var handlers = PropertyChanged;
+            if (handlers != null)
+            {
+                var args = new PropertyChangedEventArgs(property);
+                handlers(this, args);
+            }
+        }
+    }
+}
